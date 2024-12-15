061839e30bbdab17a22b10aafb304046724fb987
Maryjose <maekkovqzc@gmail.com>
2024-12-14 22:54:28 -0600

    bibliografia

--- bibliografia.php
+++ bibliografia.php
@@ -13,7 +13,7 @@
 <?php require ('ifSession.php'); ?>
 <script src="js/script11.js"></script>
     <section style="margin-top: 30px;">
-    <div class="todo">
+    <div class="todo"> 
     <div class="bibliografia">
             <ul>
                 <li>B</li>
@@ -32,49 +32,84 @@
         </div>
         <br><br><br><br>
         <div class="bibli">
-            <CENTER>
+            <div  class="textbibli">
             GCFGlobal. (s.f.). ¿Cómo simplificar expresiones algebraicas?, GCFGlobal.
-                <a href="https://edu.gcfglobal.org/es/algebra/como-simplificar-expresiones-algebraicas/1/"> https://edu.gcfglobal.org/es/algebra/como-simplificar-expresiones-algebraicas/1/ </a> <br><br>
+            <div class="LinkBibli"><a href="https://edu.gcfglobal.org/es/algebra/como-simplificar-expresiones-algebraicas/1/"> https://edu.gcfglobal.org/es/algebra/como-simplificar-expresiones-algebraicas/1/ </a> <br><br>
+            </div>
                 Rivera A.(14 de junio,2022). Características de la notación desarrollada de un número y cómo obtenerla. Luca. 
-                 <a href="https://www.lucaedu.com/notacion-desarrollada-de-un-numero/"> https://www.lucaedu.com/notacion-desarrollada-de-un-numero/ </a> <br><br>
-                 Universidad Técnica Nacional. (s.f.). Factorización de expresiones algebraicas. Campus virtual.
-                  <a href="http://ftp.campusvirtual.utn.ac.cr/calculo/Factorizacion.pdf"> http://ftp.campusvirtual.utn.ac.cr/calculo/Factorizacion.pdf </a> <br><br>
-                  Romero S. (26 de julio, 2022). Factorización. TodaMateria.  <a href="https://www.todamateria.com/factorizacion/ "> https://www.todamateria.com/factorizacion/  </a> <br><br>
-                  El blog del profe Nelson. (s.f).  Factorización de una suma o diferencia de cubos Perfectos-Álgebra. WordPress. 
-                   <a href="https://profbaptista.wordpress.com/2020/05/07/factorizacion-de-una-suma-o-diferencia-de-cubos/#:~:text=La%20diferencia%20de%20dos%20cubos,cuadrado%20de%20la%20segunda%20ra%C3%ADz"> https://profbaptista.wordpress.com/2020/05/07/factorizacion-de-una-suma-o-diferencia-de-cubos/#:~:text=La%20diferencia%20de%20dos%20cubos,cuadrado%20de%20la%20segunda%20ra%C3%ADz </a> <br><br>
+                <div class="LinkBibli"><a href="https://www.lucaedu.com/notacion-desarrollada-de-un-numero/"> https://www.lucaedu.com/notacion-desarrollada-de-un-numero/ </a> <br><br>
+            </div> 
+                Universidad Técnica Nacional. (s.f.). Factorización de expresiones algebraicas. Campus virtual.
+                 <div class="LinkBibli"><a href="http://ftp.campusvirtual.utn.ac.cr/calculo/Factorizacion.pdf"> http://ftp.campusvirtual.utn.ac.cr/calculo/Factorizacion.pdf </a> <br><br>
+                </div> 
+                 Romero S. (26 de julio, 2022). Factorización. TodaMateria.  
+                 <div class="LinkBibli"><a href="https://www.todamateria.com/factorizacion/ "> https://www.todamateria.com/factorizacion/  </a> <br><br>
+                </div> 
+                 El blog del profe Nelson. (s.f).  Factorización de una suma o diferencia de cubos Perfectos-Álgebra. WordPress. 
+                   <div class="LinkBibli"><a href="https://profbaptista.wordpress.com/2020/05/07/factorizacion-de-una-suma-o-diferencia-de-cubos/#:~:text=La%20diferencia%20de%20dos%20cubos,cuadrado%20de%20la%20segunda%20ra%C3%ADz"> https://profbaptista.wordpress.com/2020/05/07/factorizacion-de-una-suma-o-diferencia-de-cubos/#:~:text=La%20diferencia%20de%20dos %20cubos,cuadrado%20de%20la%20segunda%20ra%C3%ADz </a> <br><br>
+                   </div>
+
                     Romero S. ( 06 de diciembre, 2023). Productos notables. TodaMateria. 
-                    <a href="https://www.todamateria.com/productos-notables/"> https://www.todamateria.com/productos-notables/ </a> <br><br>
+                    <div class="LinkBibli"><a href="https://www.todamateria.com/productos-notables/"> https://www.todamateria.com/productos-notables/ </a> <br><br>
+                   </div>
+                    
                     Tus clases de mate (18 de abril, 2020). Lenguaje Natural al lenguaje algebraico y viceversa. [Vídeo] [YouTube]. 
-                     <a href="https://www.youtube.com/watch?v=6Ryn9hdDl9Y&ab_channel=TUSCLASESDEMATE%21%21%21 "> https://www.youtube.com/watch?v=6Ryn9hdDl9Y&ab_channel=TUSCLASESDEMATE%21%21%21  </a><br><br>
-                     Slideshare. (3 de septiembre, 2014). Lenguaje natural a algebraico. Slideshare. 
-                     <a href="https://es.slideshare.net/carfelaq/lenguaje-natural-a-algebraico"> https://es.slideshare.net/carfelaq/lenguaje-natural-a-algebraico   </a><br><br>
-                     Derivando. (20 de septiembre de 2023).  ¿Conoces el TEOREMA DE NAPOLEÓN? [Vídeo][YouTube]. 
-                      <a href="https://www.youtube.com/watch?v=AqHxDJ8IGX0&ab_channel=Derivando"> https://www.youtube.com/watch?v=AqHxDJ8IGX0&ab_channel=Derivando  </a><br><br>
-                      Paganerius. (15 julio, 2021). El teorema de Napoleón / El triángulo de Napoleón. [Vídeo][YouTube]. 
-                       <a href="https://www.youtube.com/watch?v=w3K5S7Vg9TY&ab_channel=paganeriu "> https://www.youtube.com/watch?v=w3K5S7Vg9TY&ab_channel=paganeriu  </a><br><br>
-                       Óscar Intriago Cedeño. (19 octubre, 2022). Sistema de inecuaciones lineales con dos incógnitas. [Vídeo][YouTube]. 
-                        <a href="https://www.youtube.com/watch?v=OYJKMTumqQI&ab_channel=%C3%93scarIntriagoCede%C3%B1o "> https://www.youtube.com/watch?v=OYJKMTumqQI&ab_channel=%C3%93scarIntriagoCede%C3%B1o  </a><br><br>
-                        Matemáticas profe Alex. (24 de mayo de 2018). Sistemas de ecuaciones lineales 2x2 | Método de Sustitución | Ejemplo 1. [Vídeo][YouTube].  
-                         <a href="https://youtu.be/LTfv1G2iYuQ?si=7-ZkWiw_0Q-mUksM "> https://youtu.be/LTfv1G2iYuQ?si=7-ZkWiw_0Q-mUksM  </a><br><br>
-                         Matemáticas profe Alex. (16 de mayo de 2018). Sistemas de ecuaciones lineales 2x2 | Método de igualación| Ejemplo 1. [Vídeo][YouTube].
-                          <a href="https://www.youtube.com/watch?v=apPXOlZnRhg&t=552s&ab_channel=Matem%C3%A1ticasprofeAlex"> https://www.youtube.com/watch?v=apPXOlZnRhg&t=552s&ab_channel=Matem%C3%A1ticasprofeAlex </a><br><br>
-                          Matemáticas profe Alex. (13 de mayo, 2018). Sistemas de ecuaciones lineales 2x2 | Método de Reducción - Eliminación | Introducción. [Vídeo][YouTube]. 
-                           <a href="https://www.youtube.com/watch?v=p2AIFY1b9qk&ab_channel=Matem%C3%A1ticasprofeAlex"> https://www.youtube.com/watch?v=p2AIFY1b9qk&ab_channel=Matem%C3%A1ticasprofeAlex 
-                           </a><br><br>
-                           Susi profe. (12 agosto, 2018). INECUACIONES con DOS Incógnitas ✅. [Vídeo] [Youtube].
-                            <a href="https://www.youtube.com/watch?v=lEh14svjac8&ab_channel=SusiProfe"> https://www.youtube.com/watch?v=lEh14svjac8&ab_channel=SusiProfe  </a><br><br>
-                            Geometriamagicomundodelorigami. (Año). Plano cartesiano [png]. WordPress. 
-                             <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeometriamagicomundodelorigami.wordpress.com%2Fplano-cartesiano%2F&psig=AOvVaw06GwYbaFkwWCC0pMt3GcFZ&ust=1702449923719000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCU_7OmiYMDFQAAAAAdAAAAABAD"> https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeometriamagicomundodelorigami.wordpress.com%2Fplano-cartesiano%2F&psig=AOvVaw06GwYba<br>FkwWCC0pMt3GcFZ&ust=1702449923719000&source=images&cd=vfe&op<br>i=89978449&ved=0CBEQjRxqFwoTCKCU_7OmiYMDFQAAAAAdAAAAABAD </a><br><br>
-                             Freire N. (01 de diciembre de 2023). El Teorema de Napoleón, o cómo el emperador francés influyó en las matemáticas. National Geographic España. 
-                              <a href="https://www.nationalgeographic.com.es/ciencia/teorema-napoleon-influencia-matematicas_21147"> https://www.nationalgeographic.com.es/ciencia/teorema-napoleon-influencia-matematicas_21147 </a><br><br>
-                              Matemáticas Profe Alex. (30 de abril de 2018). Factorización Trinomio de la forma ax2+bx+c. [vídeo][YouTube].
-                               <a href="https://www.youtube.com/watch?v=xZHGl-RUqHs"> https://www.youtube.com/watch?v=xZHGl-RUqHs </a><br><br>
-                            Matemáticas Profe Alex. (22 de noviembre de 2017). Trinomio cuadrado perfecto. [Vídeo][YouTube].
-                            <a href="https://www.youtube.com/watch?v=YAENVrFtO6E"> https://www.youtube.com/watch?v=YAENVrFtO6E </a><br><br>
-                            Matemáticas Profe Alex. (31 de mayo de 2020). Factor común por agrupación de términos Ejemplo 1 | Factorización. [Vídeo][YouTube]. 
-                             <a href="https://www.youtube.com/watch?v=y_mkvBoYz-Y"> https://www.youtube.com/watch?v=y_mkvBoYz-Y </a><br><br>
-            
-            </CENTER>
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=6Ryn9hdDl9Y&ab_channel=TUSCLASESDEMATE%21%21%21 "> https://www.youtube.com/watch?v=6Ryn9hdDl9Y&ab_channel=TUSCLASESDEMATE%21%21%21  </a><br><br>
+                   </div> 
+                    
+                    Slideshare. (3 de septiembre, 2014). Lenguaje natural a algebraico. Slideshare. 
+                    <div class="LinkBibli"><a href="https://es.slideshare.net/carfelaq/lenguaje-natural-a-algebraico"> https://es.slideshare.net/carfelaq/lenguaje-natural-a-algebraico   </a><br><br>
+                   </div> 
+                    
+                    Derivando. (20 de septiembre de 2023).  ¿Conoces el TEOREMA DE NAPOLEÓN? [Vídeo][YouTube]. 
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=AqHxDJ8IGX0&ab_channel=Derivando"> https://www.youtube.com/watch?v=AqHxDJ8IGX0&ab_channel=Derivando  </a><br><br>
+                   </div>
+                    
+                    Paganerius. (15 julio, 2021). El teorema de Napoleón / El triángulo de Napoleón. [Vídeo][YouTube]. 
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=w3K5S7Vg9TY&ab_channel=paganeriu "> https://www.youtube.com/watch?v=w3K5S7Vg9TY&ab_channel=paganeriu  </a><br><br>
+                   </div>   
+                    
+                    Óscar Intriago Cedeño. (19 octubre, 2022). Sistema de inecuaciones lineales con dos incógnitas. [Vídeo][YouTube]. 
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=OYJKMTumqQI&ab_channel=%C3%93scarIntriagoCede%C3%B1o "> https://www.youtube.com/watch?v=OYJKMTumqQI&ab_channel=%C3%93scarIntriagoCede%C3%B1o  </a><br><br>
+                   </div>
+                    
+                    Matemáticas profe Alex. (24 de mayo de 2018). Sistemas de ecuaciones lineales 2x2 | Método de Sustitución | Ejemplo 1. [Vídeo][YouTube].  
+                    <div class="LinkBibli"><a href="https://youtu.be/LTfv1G2iYuQ?si=7-ZkWiw_0Q-mUksM "> https://youtu.be/LTfv1G2iYuQ?si=7-ZkWiw_0Q-mUksM  </a><br><br>
+                   </div>
+                    
+                    Matemáticas profe Alex. (16 de mayo de 2018). Sistemas de ecuaciones lineales 2x2 | Método de igualación| Ejemplo 1. [Vídeo][YouTube].
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=apPXOlZnRhg&t=552s&ab_channel=Matem%C3%A1ticasprofeAlex"> https://www.youtube.com/watch?v=apPXOlZnRhg&t=552s&ab_channel=Matem%C3%A1ticasprofeAlex </a><br><br>
+                   </div>
+                    
+                    Matemáticas profe Alex. (13 de mayo, 2018). Sistemas de ecuaciones lineales 2x2 | Método de Reducción - Eliminación | Introducción. [Vídeo][YouTube]. 
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=p2AIFY1b9qk&ab_channel=Matem%C3%A1ticasprofeAlex"> https://www.youtube.com/watch?v=p2AIFY1b9qk&ab_channel=Matem%C3%A1ticasprofeAlex 
+                           </a><br><br></div>
+
+                    Susi profe. (12 agosto, 2018). INECUACIONES con DOS Incógnitas ✅. [Vídeo] [Youtube].
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=lEh14svjac8&ab_channel=SusiProfe"> https://www.youtube.com/watch?v=lEh14svjac8&ab_channel=SusiProfe  </a><br><br>
+                   </div>
+
+                    Geometriamagicomundodelorigami. (Año). Plano cartesiano [png]. WordPress. 
+                    <div class="LinkBibli"><a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeometriamagicomundodelorigami.wordpress.com%2Fplano-cartesiano%2F&psig=AOvVaw06GwYbaFkwWCC0pMt3GcFZ&ust=1702449923719000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCU_7OmiYMDFQAAAAAdAAAAABAD"> https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeometriamagicomundodelorigami.wordpress.com%2Fplano-cartesiano%2F&psig=AOvVaw06GwYba<br>FkwWCC0pMt3GcFZ&ust=1702449923719000&source=images&cd=vfe&op<br>i=89978449&ved=0CBEQjRxqFwoTCKCU_7OmiYMDFQAAAAAdAAAAABAD </a><br><br>
+                   </div>
+                    
+                    Freire N. (01 de diciembre de 2023). El Teorema de Napoleón, o cómo el emperador francés influyó en las matemáticas. National Geographic España. 
+                    <div class="LinkBibli"><a href="https://www.nationalgeographic.com.es/ciencia/teorema-napoleon-influencia-matematicas_21147"> https://www.nationalgeographic.com.es/ciencia/teorema-napoleon-influencia-matematicas_21147 </a><br><br>
+                   </div>          
+                    
+                    Matemáticas Profe Alex. (30 de abril de 2018). Factorización Trinomio de la forma ax2+bx+c. [vídeo][YouTube].
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=xZHGl-RUqHs"> https://www.youtube.com/watch?v=xZHGl-RUqHs </a><br><br>
+                   </div>        
+                    
+                    Matemáticas Profe Alex. (22 de noviembre de 2017). Trinomio cuadrado perfecto. [Vídeo][YouTube].
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=YAENVrFtO6E"> https://www.youtube.com/watch?v=YAENVrFtO6E </a><br><br>
+                   </div>
+                    
+                    Matemáticas Profe Alex. (31 de mayo de 2020). Factor común por agrupación de términos Ejemplo 1 | Factorización. [Vídeo][YouTube]. 
+                    <div class="LinkBibli"><a href="https://www.youtube.com/watch?v=y_mkvBoYz-Y"> https://www.youtube.com/watch?v=y_mkvBoYz-Y </a><br><br>
+                   </div>
+
+                   </div>
         </div>
 </div>
     </section>
