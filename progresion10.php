444e3f8aa4638ae1f9984989f1e312201742c39c
Maryjose <maekkovqzc@gmail.com>
2025-03-03 18:23:28 -0600

    semejanza-congruencia

--- progresion10.php
+++ progresion10.php
@@ -118,34 +118,8 @@ Teorema. Una proposición que puede ser demostrada lógicamente.<br>
 Los triángulos pueden ser tanto internos como externos, los centros de esos triángulos 
 siempre formarán un triángulo equilátero, y si restamos las áreas del triángulo externo 
 con la del triángulo interno, darán como resultado el área del triángulo original.<br><br>
-Pero ¿por qué lo llamamos “Teorema de Napoleón”? ¿No fue Napoleón un político y militar? 
-¿Por qué se nombraría a un teorema relacionado a la geometría como si lo hubiera creado él? <br><br>
 
-Bueno, a pesar de que Napoleón no fue un matemático de la antigüedad, sino que fue un militar, 
-él mostraba un gran interés en el área de las matemáticas, algo que se puede observar en una de 
-las multiples cartas de este, la cual decía “El progreso y la perfección de las matemáticas están 
-íntimamente ligadas a la prosperidad del estado” pues también era quien tenía amistad con algunos 
-matemáticos como Laplace y Fourier, siendo Napoleón quien llegaba a proponerles problemas como el 
-de dividir una circunferencia en cuatro partes iguales solamente con un compás, agregando el encontrar 
-su centro, al ya explicarles lo anterior podemos llegar al porqué se le conoce como teorema de Napoleón, 
-esto es porque como mencionamos hace unos instantes, Napoleón le proponía problemas matemáticos a sus 
-amigos matemáticos, así que un día él les propuso un problema, el cual decía que al trazar triángulos 
-equiláteros en cada lado de cualquier triángulo, los centros de esos triángulos también darían un 
-triángulo equilátero, esto lo sabemos gracias al diario “The ladies diary”, donde tuvo una mención 
-en un apartado, diciendo que dicho problema fue propuesto por Napoleón a sus amigos, con esto podemos 
-entender el porque lo llaman “Teorema de Napoleón”, no sabemos con exactitud si fue o no una creación 
-de Napoleón pero lo que si podemos confirmar es que el nombre se le quedo a partir de esa mención en 
-el diario de las damas, muchos afirman que Napoleón fue quien podría haberlo creado, mientras que otros
- cuestionan el que él no contaba con los conocimientos necesarios para plantear dicho problema y que se 
- baso a partir de libros de la epoca, donde algunos matematicos proponian algo similar.<br><br>
 
-Existen varios rumores sobre que el autor fue otro matemático de la época cercano a Napoleón, 
-el cual contaba con los conocimientos necesarios para resolverlo, sospechando que antes de 
-plantearlo a cercanos él ya sabía la respuesta, hablamos de William Rutherford, aunque volviendo 
-a lo anterior, no hay nada que confirme quien fue su verdadero autor.<br><br>
-
-Este teorema se originó gracias a la pregunta de ¿qué pasaría si en lugar de usar 
-cuadrados usamos triángulos en el teorema de Pitágoras?, o eso es lo que creemos.<br><br>
 Hay varias formas de demostrar el teorema de Napoleón, una forma sencilla y rápida es 
 al ya tener nuestro triángulo formado por los centros de los triángulos, digamos 
 que es nuestro triángulo MNL, el  lado MN, lo giramos 30° en sentido de las agujas 
@@ -182,61 +156,42 @@ Bueno, al momento de girar el lado MN y hacer la homotecia, obtenemos la línea 
             </div>
 
             <FORM METHOD=POST>
-            <p><h3>Responde las siguientes preguntas.</h3></p><br>
-            <P><b>1.- ¿Por qué se llama teorema de Napoleón?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r1" id="p10e1r1a1" value="2"> <label for="p10e1r1a1"> Él lo creó. </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r1" id="p10e1r1a2" value="1"> <label for="p10e1r1a2"> Por una mención. </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r1" id="p10e1r1a3" value="3"> <label for="p10e1r1a3"> Fue quien lo comprobó. </label><BR><br><br>
+            <p><h1>Responde las siguientes preguntas.</h1></p><br>
 
-            <P><b>2.- ¿De qué trata el teorema de Napoleón?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a1" value="1"> <label for="p10e1r2a1">Al dibujar triángulos equiláteros 
+            <P><b>1.- ¿De qué trata el teorema de Napoleón?</b></p><br>
+            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a1" value="1"> <label for="p10e1r1a1">Al dibujar triángulos equiláteros 
                 en cada uno de los lados de un triángulo cualquiera, los centros que los unen dichos 
                 triángulos formarán siempre un triángulo equilátero.</label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a2" value="2"> <label for="p10e1r2a2">La suma de los cuadrados de los 
+            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a2" value="2"> <label for="p10e1r1a2">La suma de los cuadrados de los 
                 catetos de un triángulo rectángulo es igual a la hipotenusa</label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a3" value="3"> <label for="p10e1r2a3">Toda recta paralela a un lado 
+            <INPUT TYPE="radio" name="p10e1r2" id="p10e1r2a3" value="3"> <label for="p10e1r1a3">Toda recta paralela a un lado 
                 de un triángulo, forma con los otros dos lados o con sus prolongaciones otro triángulo que 
                 es semejante al triángulo dado.</label><BR><br><br>
 
-            <P><b>3.- ¿Qué es una homotecia?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a1" value="3"> <label for="p10e1r3a1"> Una potencia. </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a2" value="2"> <label for="p10e1r3a2"> Una onomatopeya. </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a3" value="1"> <label for="p10e1r3a3"> Una ampliación. </label><BR><br><br>
+            <P><b>2.- ¿Qué es una homotecia?</b></p><br>
+            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a1" value="3"> <label for="p10e1r2a1"> Una potencia. </label> <BR><br>
+            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a2" value="2"> <label for="p10e1r2a2"> Una onomatopeya. </label><BR><br>
+            <INPUT TYPE="radio" name="p10e1r3" id="p10e1r3a3" value="1"> <label for="p10e1r2a3"> Una ampliación. </label><BR><br><br>
 
-            <P><b>4.- ¿Por cuántos grados se tiene que girar el lado 
+            <P><b>3.- ¿Por cuántos grados se tiene que girar el lado 
                 del triángulo en la forma de comprobación mencionada?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a1" value="1"> <label for="p10e1r4a1"> 30° </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a2" value="2"> <label for="p10e1r4a2"> 60° </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a3" value="3"> <label for="p10e1r4a3"> 45° </label><BR><br><br>
+            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a1" value="1"> <label for="p10e1r3a1"> 30° </label> <BR><br>
+            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a2" value="2"> <label for="p10e1r3a2"> 60° </label><BR><br>
+            <INPUT TYPE="radio" name="p10e1r4" id="p10e1r4a3" value="3"> <label for="p10e1r3a3"> 45° </label><BR><br><br>
 
-            <P><b>5.- ¿En qué sentido se tiene que girar?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r5" id="p10e1r5a1" value="1"> <label for="p10e1r5a1"> De las manecillas del reloj. </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r5" id="p10e1r5a2" value="2"> <label for="p10e1r5a2"> Contrario a las manecillas del reloj. </label><BR><br><br>
+            <P><b>4.- ¿En qué sentido se tiene que girar?</b></p><br>
+            <INPUT TYPE="radio" name="p10e1r5" id="p10e1r5a1" value="1"> <label for="p10e1r4a1"> De las manecillas del reloj. </label> <BR><br>
+            <INPUT TYPE="radio" name="p10e1r5" id="p10e1r5a2" value="2"> <label for="p10e1r4a2"> Contrario a las manecillas del reloj. </label><BR><br><br>
 
-            <P><b>6.- ¿Qué es un triángulo equilátero?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a1" value=""> <label for="p10e1r6a1"> Ninguno de sus lados son iguales. </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a2" value=""> <label for="p10e1r6a2"> El que cuenta con todos sus lados y ángulos iguales. </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a3" value=""> <label for="p10e1r6a3"> Tiene dos lados iguales. </label><BR><br><br>
+            <P><b>5.- ¿Qué es un triángulo equilátero?</b></p><br>
+            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a1" value=""> <label for="p10e1r5a1"> Ninguno de sus lados son iguales. </label> <BR><br>
+            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a2" value=""> <label for="p10e1r5a2"> El que cuenta con todos sus lados y ángulos iguales. </label><BR><br>
+            <INPUT TYPE="radio" name="p10e1r6" id="p10e1r6a3" value=""> <label for="p10e1r5a3"> Tiene dos lados iguales. </label><BR><br><br>
 
-            <P><b>7.- ¿En base a que teorema se inspiraron?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r7" id="p10e1r7a1" value="1"> <label for="p10e1r7a1"> Teorema de Pitágoras. </label> <BR><br>
-            <INPUT TYPE="radio" name="p10e1r7" id="p10e1r7a2" value="2"> <label for="p10e1r7a2"> Teorema de Tales. </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r7" id="p10e1r7a3" value="3"> <label for="p10e1r7a3"> Teorema de Euclides. </label><BR><br><br>
-
-            <P><b>8.- ¿Qué diferencia tiene el teorema de Pitágoras con el Teorema de Petr-Douglas-Neumann?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r8" id="p10e1r8a1" value="2"><label for="p10e1r8a1"> El segundo se plantea con el triángulo escaleno. </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r8" id="p10e1r8a2" value="1"><label for="p10e1r8a2"> El segundo se plantea con los triángulos isósceles. </label><BR><br><!--Correcta-->
-            <INPUT TYPE="radio" name="p10e1r8" id="p10e1r8a3" value="3"><label for="p10e1r8a3"> El segundo se planteaba con un hexágono. </label><BR><br><br>
-
-            <P><b>9.- ¿Quién fue el otro matemático que algunos creen que creó el Teorema de Napoleón?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r9" id="p10e1r9a1" value="1"><label for="p10e1r9a1"> William Rutherford </label><BR><br> <!--Correcta-->
-            <INPUT TYPE="radio" name="p10e1r9" id="p10e1r9a2" value="2"><label for="p10e1r9a2"> Laplace </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r9" id="p10e1r9a3" value="3"><label for="p10e1r9a3"> Fourier </label><BR><br><br>
-
-            <P><b>10.- ¿Cuál es una de las variantes de este teorema?</b></p><br>
-            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a1" value="3"> <label for="p10e1r10a1"> Teorema de Euclides </label><BR><br>
-            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a2" value="1"> <label for="p10e1r10a2"> Teorema de Jha Savaran </label><BR><br> <!--Correcta-->
-            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a3" value="2"> <label for="p10e1r10a3"> Teorema de Pitágoras </label><BR><br><br>
+            <P><b>6.- ¿Cuál es una de las variantes de este teorema?</b></p><br>
+            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a1" value="3"> <label for="p10e1r6a1"> Teorema de Euclides </label><BR><br>
+            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a2" value="1"> <label for="p10e1r6a2"> Teorema de Jha Savaran </label><BR><br> <!--Correcta-->
+            <INPUT TYPE="radio" name="p10e1r10" id="p10e1r10a3" value="2"> <label for="p10e1r6a3"> Teorema de Pitágoras </label><BR><br><br>
 
             <input type="submit" value="" class="botonesrevisar" name="p10e1Rev">
             <input type="reset" value="" class="botonesreintentar">
@@ -244,12 +199,45 @@ Bueno, al momento de girar el lado MN y hacer la homotecia, obtenemos la línea 
             <?php
                 if (isset($_POST['p10e1Rev'])) {
                     require('funcionrevradio.php');
-                    RevisarRadios(10, 1, 10);
+                    RevisarRadios(6, 1, 10);
                
                 }
             ?>
 
+            <h1>Triángulos, congruencia y semejanza.</h1>
+            <p>Dada la introducción al teorema de Napoleón, comencemos con el tema 
+                de triángulos, congruencia y semejanza.</p>
 
+            <p>La congruencia entre figuras se hace notal cuando los lados y ángulos coinciden.</p>
+            <img src="./../images/congruencia.png" WIDTH=30%>
+            <p>En cambio cuando son semejantes lo único que es igual son los ángulos, sus lados 
+                solamente proporcionales</p>
+
+            <p>La razón de semejanza es la razón se proporcionalidad entre los lados de la figura.</p>
+            <img src="./../images/semejanza.png" WIDTH=30%>
+            <p><h1>Criterios de semejanza:</h1></p>
+            <p><b>Criterio AA.</b><br>
+            Si dos de sus ángulos son iguales.<br>
+            <img src="./../images/semejanza-AA.png" WIDTH=30%><br>
+            <b>Criterio LLL.</b><br>
+            Si sus tres lados son proporcionales.<br>
+            <img src="./../images/semejanza-LLL.png" WIDTH=30%><br>
+            <b>Criterio LAL.</b><br>
+            Si dos de sus lados son proporcionales y el ángulo comprendido entre ellos es congruente.</p>
+            <img src="./../images/semejanza-LAL.png" WIDTH=30%><br>
+
+            <p><h1>Criterios de congruencia:</h1></p>
+            <p><b>LAL (lado, ángulo, lado)</p></b>
+            <p>Dos triángulos son congruentes cuando dos de sus ladows son iguales como el ángulo 
+            comprendido entre estos.</p>
+            <img src="./../images/congruencia-LAL.png" WIDTH=30%><br>
+            <p><b>ALA (ángulo, lado, ángulo)</p></b>
+            <p>Dos triángulos son congruentes cuando dos ángulos interiores y el lado entre ellos tienen 
+                la misma longitud y tamaño.</p>
+                <img src="./../images/congruencia-ALA.png" WIDTH=30%><br>
+            <p><b>LLL (lado, lado, lado)</p></b>
+            <p>Dos triángulos son congruentes si sus tres lados son iguales.</p>
+            <img src="./../images/congruencia-LLL.png" WIDTH=30%><br>
             <CENTER><img id="resu" src="./images/progresion10img/Progre10.png" WIDTH="100%"></CENTER>
         </section>
 
