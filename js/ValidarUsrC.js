4e950a6c1af46e81e1d0187dbb1bdb5ecd41c6bf
Maryjose <maekkovqzc@gmail.com>
2025-04-10 07:53:24 -0600

    validarContrasenaLOLOGREEEEEEE

---
+++ js/ValidarUsrC.js
@@ -0,0 +1,77 @@
+const passwordInput = document.getElementById("Passw"); //el input de la contraseña
+const Validar = document.getElementById("ValCon"); //label para colocar el texto correspondiente a la validación de la contraseña
+const btnRegistro = document.getElementById("registra"); //botón de registro del formulario 
+
+const Nombre = document.getElementById("Nombre"); //input del nombre
+const PermitirNom = document.getElementById("ValNom"); //label para el mensaje de no valido del campo
+
+const Apellido = document.getElementById("Apellido"); //input del apellido del usr
+const PermitirApe = document.getElementById("ValApe"); //label correspondiente al mensaje de no valido en caso de serlo
+
+//variables para saber el estado actual del campo llenado
+var ApeVali = false ;
+var NomVali = false ;
+var ContraVali = false;
+
+function ActualizarBoton () {
+    btnRegistro.disabled = !(ApeVali && NomVali && ContraVali); // desactiva el botón si alguna validación es falsa
+}
+
+Apellido.oninput = function(){
+    const valiApe = /^[a-zA-ZÀ-ÿñÑ\s]+$/; // expresión regular para solo aceptar letras y acentos, así como la ñ
+
+    if(Apellido.value.match(valiApe)){ //si el valor dentro del input cumple con los criterios de la expresión, entonces el botón podrá activarse
+        PermitirApe.innerHTML = "";
+        btnRegistro.disabled=false;
+        ApeVali = true ;
+    } else {
+        PermitirApe.innerHTML = "Apellido no valido";
+        btnRegistro.disabled=true;
+        ApeVali = false ;
+    }
+    ActualizarBoton(); //actualiza el estado del botón
+}
+
+Nombre.oninput = function() { //función que valida si el nombre es correcto
+    const valiNom = /^[a-zA-ZÀ-ÿñÑ\s]+$/; //expresión regular para solo admitir letras, acentos y ñ y espacios
+
+    if(Nombre.value.match(valiNom)){
+        PermitirNom.innerHTML = "";
+        btnRegistro.disabled=false; //permite que el botón se active
+        NomVali = true ; // variable para permitir activar el botón
+    } else {
+        PermitirNom.innerHTML = "Nombre no valido";
+        btnRegistro.disabled=true;
+        NomVali = false ;
+    }
+    ActualizarBoton();
+}
+
+passwordInput.oninput = function() { // función que permite validar la contraseña, de ser así, permite que el usuario utilice el botón
+    const decimal = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&{}+'¿¡\\~|°¬´´¨""#//()=``.:,;ñ])[A-Za-z\d\\@$!%*?&{}+'¿¡~|°¬´´¨""#//()=``.:,;ñ]{8,15}$/;
+//expresión regular que permite, letras (mayúsculas o mínusculas), números y carácteres especiales, si no coloca alguna entonces la contraseña no es valida
+    if(passwordInput.value.match(decimal)){
+        Validar.innerHTML = "Contraseña valida.";
+        btnRegistro.disabled=false;
+        ContraVali = true ;
+    } else {
+        Validar.innerHTML = "Contraseña invalida, se requiere una mayúscula, un signo especial, un número además de tener entre 8 y 15 carácteres.";
+        btnRegistro.disabled=true;
+        ContraVali = false;
+    }
+    ActualizarBoton();
+};
+
+
+
+function cambioOjoK (eyeicon, password){ //función que permite observar la contraseña, mediante imagenes representativas de un ojo para ser intituivo
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src= ".././images/open.png";
+    } else {
+        password.type = "password";
+        eyeicon.src= ".././images/close.png";
+    }
+    
+}
+
--- registro.php
+++ registro.php
@@ -17,8 +17,6 @@
     include('ifSession.php');
     ?>
 
-<script src="js/validarContra.js"></script>
-<script src="js/osalesosales.js"></script>
 
     <section style=" margin-top:30px;">
         <div class="formulario2">
@@ -31,11 +29,13 @@
                 <div id="contin2">
 
                     <div class="inputcamp">
-                        <Input type="text" name="Nombre" placeholder="Nombre" required><br>
+                        <Input type="text" id="Nombre" name="Nombre" placeholder="Nombre" required><br>
+                        <label id="ValNom"></label>
                     </div>
 
                     <div class="inputcamp">
-                        <Input type="text" name="Apellido" placeholder="Apellido" required> <br>
+                        <Input type="text" id="Apellido" name="Apellido" placeholder="Apellido" required> <br>
+                        <label id="ValApe"></label>
                     </div>
 
                     <div class="inputcamp">
@@ -45,9 +45,10 @@
                     <div class="inputcamp">
                         <Input type="password" name="Passw" id="Passw" placeholder="Contrasena" minlength=8 required>
                         <img src=".././images/close.png" id="eyeicon" onclick="cambioOjoK(document.registro.eyeicon, document.registro.Passw)"> <br>
-                        <label id="ValCon">Validar contraseña.</label><button type=button id="comprobar" onclick="validarPassword(document.registro.Passw, document.registro.registra)"></button>
+                        <label id="ValCon"></label>
                         
-                    </div>
+                    </div><br><br>
+                    
                     <div class="recuerdame">
                         <Input class="recuerdame" type="checkbox" name="Mantener" id="SesionM"><label
                             for="SesionM">Mantener inicio de sesion</label>
@@ -55,7 +56,8 @@
                     
                     <p class="new">¿Ya tienes una cuenta? <a href="sesion.php">Da click aqui para iniciar sesion</a>
                     </p><br><br>
-                    <input type="submit" id="registra" name="iniciaS" value="Registrarse" class="btn" disabled=true> <!--onclick="validarPassword(document.registro.Passw)"-->
+                    <input type="submit" id="registra" name="iniciaS" value="Registrarse" class="btn" disabled=true>
+                    <script src="js/ValidarUsrC.js"></script>
                 </div>
 
 
