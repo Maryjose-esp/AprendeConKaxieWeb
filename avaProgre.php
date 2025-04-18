6219b159c1ecef6ca3e916fe8a11e29473b595ca
Maryjose <maekkovqzc@gmail.com>
2025-04-18 13:17:21 -0600

    AVANCEPROGRESION

---
+++ avaProgre.php
@@ -0,0 +1,27 @@
+<?php 
+if (isset($_SESSION['Usuario'])) {
+    require('conexionbd.php');
+    $usuario = $_SESSION['Usuario'];
+
+    $conexion=mysqli_connect($db_host, $db_usuario, $db_contrasena, $db_nombre);
+    mysqli_select_db($conexion,$db_nombre) or die ("No se encuentra la base de datos");
+    mysqli_set_charset($conexion,"utf8");
+
+    $sql= "SELECT $progresion FROM avances WHERE nusuario=?";
+    $resultado=mysqli_prepare($conexion,$sql);
+    
+    $ok= mysqli_stmt_bind_param($resultado,"s",$usuario);
+    $ok= mysqli_stmt_execute($resultado);
+    
+    if ($ok==false){
+        
+        echo "Error al ejecutar consulta";
+    } else {
+        $ok=mysqli_stmt_bind_result($resultado,$progre);
+        while (mysqli_stmt_fetch($resultado)){
+            echo $progre;
+        }
+    }
+    mysqli_stmt_close($resultado);
+}
+?>
--- avances.php
+++ avances.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style5.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="./../images/logo.ico" type= "image/x-icon">
     <title>Mi avance - Aprende Con Kaxie</title>
@@ -17,118 +18,240 @@
     <BR>
     <div class="progre">
         <CENTER><h1>Progresión 1 &emsp; <a href="progresion1.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P1.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P1.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p1";
+            require("avaProgre.php");
+        ?>
+        % </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style='width: 
+            <?php 
+                $progresion="p1";
+                require("avaProgre.php");
+            ?>%;'></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 2 &emsp; <a href="progresion2.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P2.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P2.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p2";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style='width:
+            <?php 
+                $progresion="p2";
+                require("avaProgre.php");
+            ?>%;'></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 3 &emsp; <a href="progresion3.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P3.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P3.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p3";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p3";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 4 &emsp; <a href="progresion4.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P4.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P4.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p4";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p4";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 5 &emsp; <a href="progresion5.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P5.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P5.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p5";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p5";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
-        <CENTER><h1>Progresión 6 &emsp; <a href="progresion7.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P6.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <CENTER><h1>Progresión 6 &emsp; <a href="progresion6.php">Ir a la progresión</a></h1></CENTER>
+        <img src="images/P6.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p6";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p6";
+                require("avaProgre.php");
+            ?>%;"></div>
+        </div>
+    </div>
+
+    <div class="progre">
+        <CENTER><h1>Progresión 7 &emsp; <a href="progresion7.php">Ir a la progresión</a></h1></CENTER>
+        <img src="images/P7.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p7";
+            require("avaProgre.php");
+        ?>% </div>
+        <div class="porce" >
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p7";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
  
     <div class="progre">
         <CENTER><h1>Progresión 8 &emsp; <a href="progresion8.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P8.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P8.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p8";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p8";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 9 &emsp; <a href="progresion9.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P9.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P9.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p9";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p9";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 10 &emsp; <a href="progresion10.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P10.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P10.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p10";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p10";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 11 &emsp; <a href="progresion11.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P11.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P11.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p11";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p11";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 12 &emsp; <a href="progresion12.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P12.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P12.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p12";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p12";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 13 &emsp; <a href="progresion13.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P13.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P13.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p13";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p13";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
     <div class="progre">
         <CENTER><h1>Progresión 14 &emsp; <a href="progresion14.php">Ir a la progresión</a></h1></CENTER>
-        <img src="./../images/P14.png" WIDTH=35%> 
-        <div class="avance">Tu avance es de 0% </div>
+        <img src="images/P14.png" WIDTH=35%> 
+        <div class="avance">Tu avance es de 
+        <?php    
+        $progresion="p14";
+            require("avaProgre.php");
+        ?>% </div>
         <div class="porce" >
-            <div class="fill a" style="width: 0%;"></div>
+            <div class="fill a" style="width:  
+            <?php 
+                $progresion="p14";
+                require("avaProgre.php");
+            ?>%;"></div>
         </div>
     </div>
 
